a5e61b91cf9c2137e41568ee0250bcc36f0401e6
angela wang <140568623+alegnaaa-hd@users.noreply.github.com>
2024-07-16 10:28:00 -0400

    Update basic_calculator.py

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,5 +1,38 @@
+# DSAA Week 1 Day 3, 7/16/2024
+
+from calculatorPY.functions import add, subtract, multiply, divide
 
 number1 = input("Enter the first number: ")
 number2 = input("Enter the second number: ")
+print ("Options: ")
+print ("1. 'addition'")
+print ("2. 'subtraction'")
+print ("3. 'multiplication'")
+print ("4. 'division'")
+operation = str(input("Enter operation name: "))
 
-print ("You entered ", number1, " and ", number2)
+a = "addition"
+s = "subtraction"
+m = "multiplication"
+d = "division"
+
+x = float(number1)
+y = float(number2)
+
+if operation == a:
+    result = add(x,y)
+
+elif operation == s:
+    result = subtract(x,y)
+
+elif operation == m:
+    result = multiply(x,y)
+
+elif operation == d:
+    result = multiply(x,y)
+
+else:
+    print ("That is not an option")
+
+print (number1, " ", operation, " ", number2, " = ", result)
+
