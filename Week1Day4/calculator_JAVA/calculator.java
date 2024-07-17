bb9749b4933a1b8bcda2e419eac461b130092478
Angela Wang <angelavveivvei@gmail.com>
2024-07-17 10:24:38 -0400

    organize

--- Week1Day4/calculator_JAVA/calculator.java
+++ Week1Day4/calculator_JAVA/calculator.java
@@ -1,6 +1,6 @@
 // DSAA Week 1 Day 3-4, 7/16/2024
 
-package calculator_JAVA;
+package Week1Day4.calculator_JAVA;
 
 public class calculator 
 {
@@ -8,6 +8,7 @@ public class calculator
     {
         String line = "---------------------------------";
         String bar =  "           - - - - -";
+        
     }    
 
     private static void menuOptions()
