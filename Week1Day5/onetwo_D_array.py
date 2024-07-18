a937a660e18bf206cc1acc6c15f7a2b076cf8971
Angela Wang <angelavveivvei@gmail.com>
2024-07-18 12:48:57 -0400

    1D + 2D array in python

---
+++ Week1Day5/onetwo_D_array.py
@@ -0,0 +1,27 @@
+# DSAA Week 1 Day 5, 7/18/2024
+
+# 1 and 2 dimensional arrays 
+
+# first array
+one_d = list()
+one_d.append(1)
+one_d.append("hello")
+one_d.append(3.5)
+one_d.append(5.0)
+one_d.append('c')
+
+# second array
+another_one_d = []
+another_one_d.append("hi")
+another_one_d.append("1")
+another_one_d.append(6.58)
+
+# new array to make 2D array
+two_d = []
+
+# make 2D array
+two_d.append(one_d)
+two_d.append(another_one_d)
+
+print (two_d)
+
