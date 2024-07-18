b1ccaa077af6afef173abb4ad033308ed8f83c0b
Angela Wang <angelavveivvei@gmail.com>
2024-07-18 12:28:19 -0400

    fibonacci memoization

---
+++ Week1Day5/fib_memoization.java
@@ -0,0 +1,26 @@
+// DSAA Week 1 Day 5, 7/18/2024
+
+// calculate fibonacci sequence and store in array
+
+public class fib_memoization 
+{
+    public static void main (String[] args)
+    {
+        int fib[] = new int[300000000];
+
+        // define first two elements of the fibonacci sequence
+        fib[0] = 0;
+        fib[1] = 1;
+
+        System.out.println(fib[0]);
+        System.out.println(fib[1]);
+
+        // calculate other values based on first two
+        // 300 million fibonacci elements 
+        for (int i = 2; i < 300000000; i++)
+        {
+            fib[i] = fib[i-1] + fib[i-2];
+            System.out.println(fib[i]);
+        }
+    }
+}
