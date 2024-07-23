a5e037f188b4778c72815a3eb0c68af16e33420d
Angela Wang <angelavveivvei@gmail.com>
2024-07-23 14:44:19 -0400

    finalize selection sort

--- Week2Day3/SS_selectionsort/selectionsort.java
+++ Week2Day3/SS_selectionsort/selectionsort.java
@@ -6,7 +6,7 @@
  * this is my final attempt (hopefully)
  * 
  * YAY
- */
+*/
 
 package SS_selectionsort;
 
@@ -22,21 +22,24 @@ public class selectionsort {
 
         for (int i = 0; i < nums.length; i++)
         {
+            // smallest index to i because we use it later in for loop
             smallestIndex = i;
             for (int j = (i + 1); j < nums.length; j++)
             {
                 // if a smaller value is found
                 if (nums[i] > nums[j] && nums[smallestIndex] > nums[j])
                 {
+                    // update the smallest index
                     smallestIndex = j;
                 }
             }
-            System.out.println("smallest index: " + smallestIndex);
             
             numToChange = nums[smallestIndex];
             nums[smallestIndex] = nums[i];
             nums[i] = numToChange;
         }
+
+        // print out the array
         for (int i = 0; i < nums.length; i++)
         {
             System.out.println(nums[i]);
